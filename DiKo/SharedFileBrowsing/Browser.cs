2ee11d2ad774a9dca6f153f99efa37579d73792b
BiroMarton <rezoner9@gmail.com>
2017-08-16 13:40:13 +0200

    Created GetUsers method.

---
+++ DiKo/SharedFileBrowsing/Browser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace DiKo.SharedFileBrowsing
+{
+    class Browser
+    {
+
+        public SqlConnection GetConnenction()
+        {
+            SqlConnection con = new SqlConnection("Data Source = .;Initial Catalog = domain;Integrated Security = True");
+            return con;
+        }
+
+        public List<string> GetUsers()
+        {
+            List<string> userList = new List<string>();
+            SqlDataReader myReader = null;
+            SqlConnection con = GetConnenction();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select users from tablename", con);
+            myReader = cmd.ExecuteReader();
+            while (myReader.Read())
+            {
+                userList.Add(myReader["Column1"].ToString());
+            }
+            con.Close();
+            return userList;
+        }
+    }
+}
