7eac16f1150b64c539cf8c1b87ebacda22e9c2fe
Norbert Lugosi <lugosi.norbert@hotmail.com>
2017-08-17 15:35:17 +0200

    ItemsSharedWithMe Panel 95% done

--- DiKo/MenuWindow.xaml.cs
+++ DiKo/MenuWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace DiKo
 {
@@ -20,14 +21,72 @@ namespace DiKo
     /// </summary>
     public partial class MenuWindow : Window
     {
+        LoadingScreen loadingScreen = new LoadingScreen();
         public MenuWindow()
         {
             InitializeComponent();
             this.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
             FileData datagrid = new FileData(sharedGrid);
+            FileData datagridShared = new FileData(itemsSharedWithMeGrid);
             datagrid.createDataGrid();
+            datagridShared.createDataGrid();
             Treeview tree = new Treeview(whatToShareTreeView, sharedGrid);
             tree.Window_Loaded();
+        }
+
+        private void StartCloseTimer()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(3d);
+            timer.Tick += TimerTick;
+            timer.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            DispatcherTimer timer = (DispatcherTimer)sender;
+            timer.Stop();
+            timer.Tick -= TimerTick;
+            loadingScreen.Hide();
+            MessageBox.Show("Shared Files Added!");
+        }
+
+        private void shareButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            StartCloseTimer();
+            loadingScreen.Show();
+            loadingScreen.Topmost = true;
+        }
+
+        private void shareButtonLabel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            StartCloseTimer();
+            loadingScreen.Show();
+            loadingScreen.Topmost = true;
+
+
+        }
+
+        private void sharedItemsButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            sharedContentPanel.Visibility = Visibility.Visible;
+            sharedTreeViewPanel.Visibility = Visibility.Visible;
+            itemsSharedWithMePanel.Visibility = Visibility.Hidden;
+            SharedDataGrid.Background = Brushes.DimGray;
+            TreeGrid.Background = Brushes.DarkGray;
+            sharedWithMe.Background = Brushes.Transparent;
+            ShareButtonPanel.Visibility = Visibility.Visible;
+
+
+        }
+
+        private void itemsSharedWithMeButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            sharedContentPanel.Visibility = Visibility.Hidden;
+            sharedTreeViewPanel.Visibility = Visibility.Hidden;
+            itemsSharedWithMePanel.Visibility = Visibility.Visible;
+            sharedWithMe.Background = Brushes.DimGray;
+            ShareButtonPanel.Visibility = Visibility.Hidden;
         }
     }
 }
